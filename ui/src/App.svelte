3de378ee655bf489ca8d464269af3a012fa72eba
nmcclay <sirnikkio@gmail.com>
2022-02-26 17:25:08 -0500

    Rest of update to svelte

---
+++ ui/src/App.svelte
@@ -0,0 +1,39 @@
+<script>
+	import { Router, Link, Route } from "svelte-navigator";
+	import Quizzes from "./routes/Quizzes.svelte";
+	import Quiz from "./routes/Quiz.svelte";
+	export let name;
+</script>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
+
+<Router>
+	<nav>
+		<Link to="/">Home</Link>
+	</nav>
+	<main>
+		<Route path="/">
+			<Quizzes name={name} />
+		</Route>
+		<Route path="quiz/:id" component={Quiz} />
+	</main>
+</Router>
+
+
